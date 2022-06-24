8bd9b062049af28e3aebe422ae63b1e9a9b28004
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-06-24 09:12:06 +0200

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'http://sis-backend.us-east-1.elasticbeanstalk.com/api'
+  baseURL: 'http://sis-backend.us-east-1.elasticbeanstalk.com/api',
+  driveURL: 'http://drive.us-east-1.elasticbeanstalk.com/api/'
 };
