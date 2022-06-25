59311612296b6f0221d6700cfe9865854b15c914
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-06-25 11:09:59 +0200

    deploy

--- src/app/college-management/component/colleges-list/colleges-list.component.ts
+++ src/app/college-management/component/colleges-list/colleges-list.component.ts
@@ -15,154 +15,152 @@ import {LoadDataEvent} from '../../../shared/model/load-data-event';
 import {Constants} from '../../../shared/constants';
 
 @Component({
-  selector: 'app-colleges-list',
-  templateUrl: './colleges-list.component.html',
-  styleUrls: ['./colleges-list.component.css'],
-  providers: [MessageService, ConfirmationService]
+   selector: 'app-colleges-list',
+   templateUrl: './colleges-list.component.html',
+   styleUrls: ['./colleges-list.component.css'],
+   providers: [MessageService, ConfirmationService]
 })
 export class CollegesListComponent implements OnInit, OnDestroy {
 
-  @ViewChild('paginator') paginator: MatPaginator;
-  tableData: PageRequest<CollegeModel>;
-  collegeRequestModel: CollegeRequestModel = new CollegeRequestModel();
-  displayedColumns = ['id', 'nameEn', 'nameAr', 'code', 'Actions'];
-  pageIndex = 0;
-  pageSize = 5;
-  subscriptionsList: Subscription[] = [];
-  isSmallScreen: boolean;
-  collegeModel = new CollegeModel();
-  loading = true;
-  firstTime = true;
+   @ViewChild('paginator') paginator: MatPaginator;
+   tableData: PageRequest<CollegeModel>;
+   collegeRequestModel: CollegeRequestModel = new CollegeRequestModel();
+   displayedColumns = ['id', 'nameEn', 'nameAr', 'code', 'Actions'];
+   pageIndex = 0;
+   pageSize = 5;
+   subscriptionsList: Subscription[] = [];
+   isSmallScreen: boolean;
+   collegeModel = new CollegeModel();
+   loading = true;
+   firstTime = true;
 
 
-  constructor(private messageService: MessageService, private confirmationService: ConfirmationService,
-              private collegeManagementService: CollegeManagementService,
-              private modalService: BsModalService,
-              private breakpointObserver: BreakpointObserver,
-              private snackBar: MatSnackBar,
-              private router: Router, private primengConfig: PrimeNGConfig) {
-  }
+   constructor(private messageService: MessageService, private confirmationService: ConfirmationService,
+               private collegeManagementService: CollegeManagementService,
+               private modalService: BsModalService,
+               private breakpointObserver: BreakpointObserver,
+               private snackBar: MatSnackBar,
+               private router: Router, private primengConfig: PrimeNGConfig) {
+   }
 
-  ngOnInit(): void {
-    this.subscriptionsList = this.subscriptions();
-    this.primengConfig.ripple = true;
-  }
+   ngOnInit(): void {
+      this.subscriptionsList = this.subscriptions();
+      this.primengConfig.ripple = true;
+   }
 
 
-  pageChangeEvent(event: PageEvent): void {
-    this.collegeManagementService.getCollegePage(event.pageIndex, event.pageSize, this.collegeRequestModel)
-      .subscribe(value => {
-        this.tableData = value;
+   pageChangeEvent(event: PageEvent): void {
+      this.collegeManagementService.getCollegePage(event.pageIndex, event.pageSize, this.collegeRequestModel)
+         .subscribe(value => {
+            this.tableData = value;
+         });
+   }
+
+   addOrUpdateCollege(college: CollegeModel): void {
+      if (this.isSmallScreen) {
+         this.router.navigateByUrl('/colleges-management/create-college', {state: college}).then(_ => console.log());
+      } else {
+         const initialState = {
+            collegeModel: college
+         };
+         this.modalService.show(SaveCollegeComponent, {initialState});
+      }
+   }
+
+
+   viewCollege(row: CollegeModel): void {
+
+   }
+
+   deleteCollege(id: number): void {
+      this.confirmationService.confirm({
+         message: 'Delete College',
+         header: '',
+         accept: () => {
+            this.collegeManagementService.deleteCollege(id).subscribe(_ => {
+               this.messageService.add({severity: 'success', summary: 'Success', detail: 'College Deleted'});
+               this.loading = true;
+               this.collegeManagementService.getCollegePage(this.pageIndex, this.pageSize, this.collegeRequestModel).subscribe(value => {
+                  this.tableData = value;
+                  this.loading = false;
+               });
+            }, _ => {
+               this.messageService.add({severity: 'error', summary: 'Error', detail: 'Deletion Failed'});
+            });
+         },
+         reject: () => {
+         }
       });
-  }
+   }
 
-  addOrUpdateCollege(college: CollegeModel): void {
-    if (this.isSmallScreen) {
-      this.router.navigateByUrl('/colleges-management/create-college', {state: college}).then(_ => console.log());
-    } else {
-      const initialState = {
-        collegeModel: this.collegeModel
-      };
-      this.modalService.show(SaveCollegeComponent, {initialState});
-    }
-  }
+   saveCollege(): Subscription {
+      return this.collegeManagementService.collegeSaveEvent.subscribe(value => {
+         this.collegeManagementService.saveCollege(value).subscribe(data => {
+            console.log(data);
+         });
+      });
+   }
 
+   private subscriptions(): Subscription[] {
+      this.subscriptionsList.push(this.initialDataSubscription());
+      this.subscriptionsList.push(this.filterEventSubscription());
+      this.subscriptionsList.push(this.breakpointObserver.observe(Breakpoints.Handset).subscribe(value => {
+         this.isSmallScreen = value.matches;
+      }));
+      this.subscriptionsList.push(this.saveCollege());
+      return this.subscriptionsList;
+   }
 
-  viewCollege(row: CollegeModel): void {
+   loadData(event: LoadDataEvent): void {
+      this.loading = true;
+      this.collegeRequestModel.sortBy = event.sortField !== undefined ? event.sortField : this.collegeRequestModel.sortBy;
+      this.collegeRequestModel.sortDirection = event.sortOrder === 1 ? Constants.ASC : Constants.DESC;
+      if (this.firstTime) {
+         this.loading = false;
+         this.firstTime = false;
+      } else {
+         this.collegeManagementService.getCollegePage(
+            this.pageIndex, this.pageSize, this.collegeRequestModel).subscribe(value => {
+            this.loading = false;
+            this.tableData = value;
+         });
+      }
+   }
 
-  }
+   paginate(page: any): void {
+      this.loading = true;
+      this.pageIndex = page.page;
+      this.pageSize = page.rows;
+      this.collegeManagementService.getCollegePage(this.pageIndex, this.pageSize, this.collegeRequestModel).subscribe(value => {
+         this.loading = false;
+         this.tableData = value;
+      });
+   }
 
-  deleteCollege(id: number): void {
-    this.confirmationService.confirm({
-      message: 'Delete College',
-      header: 'test',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.collegeManagementService.deleteCollege(id).subscribe(_ => {
-          this.messageService.add({severity: 'success', summary: 'Success', detail: 'College Deleted'});
-          this.loading = true;
-          this.collegeManagementService.getCollegePage(this.pageIndex, this.pageSize, this.collegeRequestModel).subscribe(value => {
+   ngOnDestroy(): void {
+      this.subscriptionsList.forEach(sub => sub.unsubscribe());
+   }
+
+   private filterEventSubscription(): Subscription {
+      return this.collegeManagementService.collegeFilterEvent
+         .subscribe(value => {
+            this.collegeRequestModel = value;
+            this.collegeManagementService
+               .getCollegePage(0, this.pageSize, this.collegeRequestModel)
+               .subscribe(filteredData => {
+                  this.tableData = filteredData;
+               });
+         });
+   }
+
+   private initialDataSubscription(): Subscription {
+      this.loading = true;
+      return this.collegeManagementService
+         .getCollegePage(0, this.pageSize, this.collegeRequestModel)
+         .subscribe(value => {
             this.tableData = value;
             this.loading = false;
-          });
-        }, _ => {
-          this.messageService.add({severity: 'error', summary: 'Error', detail: 'Deletion Failed'});
-        });
-      },
-      reject: () => {
-      }
-    });
-  }
-
-  saveCollege(): Subscription {
-    return this.collegeManagementService.collegeSaveEvent.subscribe(value => {
-      this.collegeManagementService.saveCollege(value).subscribe(data => {
-        console.log(data);
-      });
-    });
-  }
-
-  private subscriptions(): Subscription[] {
-    this.subscriptionsList.push(this.initialDataSubscription());
-    this.subscriptionsList.push(this.filterEventSubscription());
-    this.subscriptionsList.push(this.breakpointObserver.observe(Breakpoints.Handset).subscribe(value => {
-      this.isSmallScreen = value.matches;
-    }));
-    this.subscriptionsList.push(this.saveCollege());
-    return this.subscriptionsList;
-  }
-
-  loadData(event: LoadDataEvent): void {
-    this.loading = true;
-    this.collegeRequestModel.sortBy = event.sortField !== undefined ? event.sortField : this.collegeRequestModel.sortBy;
-    this.collegeRequestModel.sortDirection = event.sortOrder === 1 ? Constants.ASC : Constants.DESC;
-    if (this.firstTime) {
-      this.loading = false;
-      this.firstTime = false;
-    } else {
-      this.collegeManagementService.getCollegePage(
-        this.pageIndex, this.pageSize, this.collegeRequestModel).subscribe(value => {
-        this.loading = false;
-        this.tableData = value;
-      });
-    }
-    console.log('lazy load ', this.collegeRequestModel);
-  }
-
-  paginate(page: any): void {
-    this.loading = true;
-    this.pageIndex = page.page;
-    this.pageSize = page.rows;
-    this.collegeManagementService.getCollegePage(this.pageIndex, this.pageSize, this.collegeRequestModel).subscribe(value => {
-      this.loading = false;
-      this.tableData = value;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptionsList.forEach(sub => sub.unsubscribe());
-  }
-
-  private filterEventSubscription(): Subscription {
-    return this.collegeManagementService.collegeFilterEvent
-      .subscribe(value => {
-        this.collegeRequestModel = value;
-        this.collegeManagementService
-          .getCollegePage(0, this.pageSize, this.collegeRequestModel)
-          .subscribe(filteredData => {
-            this.tableData = filteredData;
-          });
-      });
-  }
-
-  private initialDataSubscription(): Subscription {
-    this.loading = true;
-    return this.collegeManagementService
-      .getCollegePage(0, this.pageSize, this.collegeRequestModel)
-      .subscribe(value => {
-        this.tableData = value;
-        this.loading = false;
-      });
-  }
+         });
+   }
 }
 
