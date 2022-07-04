569d862c4fa1623119a6dce000327ceb362b3e60
Eslam-Refaey <erefaey97@gmail.com>
2022-07-05 00:20:15 +0200

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  baseURL: 'http://localhost:8080/api',
-  driveURL: 'http://localhost:8081/api/'
+  baseURL: 'http://sis-backend.us-east-1.elasticbeanstalk.com/api',
+  driveURL: 'http://drive.us-east-1.elasticbeanstalk.com/api/'
 };
 
 /*
